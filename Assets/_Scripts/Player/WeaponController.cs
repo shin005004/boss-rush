caf2f25dd3d805021655f5e84fa1763c20de652c
shin005004 <39648620+shin005004@users.noreply.github.com>
2024-01-30 05:36:31 +0900

    플레이어 값 수정

--- Assets/_Scripts/Player/WeaponController.cs
+++ Assets/_Scripts/Player/WeaponController.cs
@@ -82,7 +82,7 @@ public class WeaponController : MonoBehaviour
         attackPreInputFlag = true;
         _swordWeapon.WeaponAnimator.SetBool(boolName, false);
 
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.25f);
         attackCoolFlag = true;        
     }
 
