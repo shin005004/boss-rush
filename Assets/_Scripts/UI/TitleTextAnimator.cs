1998d4b9da18ec54f3dfa9d1750e8a63f3fd85cb
shin005004 <39648620+shin005004@users.noreply.github.com>
2024-02-16 00:16:48 +0900

    Title

---
+++ Assets/_Scripts/UI/TitleTextAnimator.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class TitleTextAnimator : MonoBehaviour
+{
+
+    [SerializeField] private TMP_Text titleText3;
+    private void Start()
+    {
+        StartCoroutine(TitleLine());
+    }
+
+    private IEnumerator TitleLine()
+    {
+        yield return new WaitForSeconds(0.5f);
+        yield return StartCoroutine(TextColor(titleText3));
+
+        StartCoroutine(TextBlinking(titleText3));
+    }
+
+    private IEnumerator TextColor(TMP_Text text)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < 0.7f)
+        {
+            elapsedTime += Time.deltaTime;
+            text.color = new Color(text.color.r, text.color.g, text.color.b, elapsedTime / 0.6f);
+            yield return null;
+        }
+        text.color = new Color(text.color.r, text.color.g, text.color.b, 1f);
+    }
+
+    private IEnumerator TextBlinking(TMP_Text text)
+    {
+        float elapsedTime = 0f;
+        while (true)
+        {
+            elapsedTime += Time.deltaTime;
+            text.color = new Color(text.color.r, text.color.g, text.color.b, 2f * (Mathf.Sin(elapsedTime) + 1f) / 2f);
+            yield return null;
+        }
+    }
+}
