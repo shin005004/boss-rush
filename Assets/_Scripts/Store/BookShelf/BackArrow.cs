a29b65e1d365bf2da80ece216915a38fcb6e8dd8
jhoh0518 <129178006+jhoh0518@users.noreply.github.com>
2024-02-17 21:47:29 +0900

    Add backarrow guide

--- Assets/_Scripts/Store/BookShelf/BackArrow.cs
+++ Assets/_Scripts/Store/BookShelf/BackArrow.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class BackArrow : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerClickHandler
 {
+    [SerializeField] private GameObject guide;
     private Image image;
 
     private void Awake()
@@ -16,11 +17,13 @@ public class BackArrow : MonoBehaviour, IPointerEnterHandler, IPointerExitHandle
     public void OnPointerEnter(PointerEventData eventData)
     {
         image.color = new Color(169 / 255f, 169 / 255f, 169 / 255f, 1f);
+        guide.SetActive(true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         image.color = Color.white;
+        guide.SetActive(false);
     }
 
     public void OnPointerClick(PointerEventData eventData)
