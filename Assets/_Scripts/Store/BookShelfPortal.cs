e515f7102bfa11d6c3d34e647b185c1c99292036
jhoh0518 <129178006+jhoh0518@users.noreply.github.com>
2024-02-17 21:38:43 +0900

    Add Key Info

--- Assets/_Scripts/Store/BookShelfPortal.cs
+++ Assets/_Scripts/Store/BookShelfPortal.cs
@@ -4,20 +4,36 @@ using UnityEngine;
 
 public class BookShelfPortal : MonoBehaviour
 {
+    [SerializeField] private GameObject pressE;
     private GameObject bookShelf;
     private string destination;
+    private bool onPortal;
+    public bool OnPortal => onPortal;
 
     private void Start(){
         bookShelf = gameObject.transform.parent.gameObject;
         destination = bookShelf.GetComponent<MiniBookShelf>().destination;
     }
 
+    private void OnTriggerEnter2D(Collider2D other){
+        if(other.transform.root.name.Equals("Player")){
+            pressE.transform.position = gameObject.transform.position + new Vector3(0f, 1f, 0f);
+        }
+    }
+
     private void OnTriggerStay2D(Collider2D other){
         if(other.transform.root.name.Equals("Player")){
+            pressE.SetActive(true);
             if(Input.GetKey(KeyCode.E)){
                 GameManager.Instance.BookManager.SetBookRoomType(destination);
                 SceneLoader.Instance.LoadBookShelfScene();
             }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other){
+        if(other.transform.root.name.Equals("Player")){
+            pressE.SetActive(false);
         }
     }
 }
--- Assets/_Scripts/Store/WorldmapPortal.cs
+++ Assets/_Scripts/Store/WorldmapPortal.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class WorldmapPortal : MonoBehaviour
 {
+    [SerializeField] private GameObject pressE;
     private void OnTriggerStay2D(Collider2D other){
         if(other.transform.root.name.Equals("Player")){
+            pressE.SetActive(true);
             if(Input.GetKey(KeyCode.E)){
                 Debug.Log("World Map");
                 MapUI.MapAppear = true;
             }
+        }
+    }
+        private void OnTriggerEnter2D(Collider2D other){
+        if(other.transform.root.name.Equals("Player")){
+            pressE.transform.position = gameObject.transform.position + new Vector3(0f, 1f, 0f);
+        }
+    }
+
+
+    private void OnTriggerExit2D(Collider2D other){
+        if(other.transform.root.name.Equals("Player")){
+            pressE.SetActive(false);
         }
     }
 }
